c1a4695f11a1e94b8da26794cf70094a48014242
maheshwari20 <84080708+maheshwari20@users.noreply.github.com>
2021-07-02 14:45:57 +0530

    create positive numbers.py

---
+++ positivenumbers.py
@@ -0,0 +1,21 @@
+python program on printing all positive numbers
+
+# list of numbers
+list1 = [11, -21, 0, 45, 66, -93]
+# list of numbers
+list2 = [12, 14, -95, 3]
+
+  
+# iterating each number in list
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+  
+# iterating each number in list
+for num in list2:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
